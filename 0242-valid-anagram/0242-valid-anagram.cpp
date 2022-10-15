0b64b760e30d008a6937eb604877610c28bb694e
plabon1803086 <79649414+plabon1803086@users.noreply.github.com>
2022-10-16 01:22:43 +0600

    Time: 17 ms (59.07%), Space: 7.3 MB (56.06%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
       
-       sort(s.begin(),  s.end());
-       sort(t.begin(),  t.end());
+        if( s.length()!=t.length()){
+            return false;
+        }
+        int ara[26] = {0};
+        int ara2[26] = {0};
         
-        if(s==t){
-            return true;
+        for(int i=0; i<s.length(); i++){
+            ara[s[i]-'a']++;
         }
-        else{
-            return false        ;
+         for(int i=0; i<t.length(); i++){
+            ara2[t[i]-'a']++;
         }
+        
+          for(int i=0; i<26; i++){
+            if(ara[i]!=ara2[i]){
+                return false;
+            }
+        }
+        
+        return true;
+        
+        
+        
+        
         
     }
 };
